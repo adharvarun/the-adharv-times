3761c5d4e76ec46983676f5cf2c8195ca13d938c
Adharv Arun <adharvarun.10@gmail.com>
2025-05-08 21:32:15 +0400

    modify for mobile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
 <footer className="w-full bg-[#0e1013] text-white py-2 border-t border-gray-800 shadow-inner flex-shrink-0">
       <div className="max-w-5xl mx-auto px-6 py-6 text-center text-gray-600 dark:text-gray-400 text-sm">
-        &copy; {new Date().getFullYear()} The Adharv Times. All rights reserved.
+        <p>Made with ❤️ by <a href="https://github.com/adharvarun" target="_blank" rel="noopener noreferrer">Adharv Arun</a></p>
       </div>
     </footer>
   );
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,11 +7,11 @@ export default function Header() {
       <div className="max-w-4xl mx-auto px-4 grid grid-cols-1 gap-2 text-center">
         <Link href="/" className="flex items-center justify-center space-x-3">
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
-          <span className="text-3xl font-bold tracking-tight font-dm-serif-display">
+          <span className="hidden sm:inline text-3xl font-bold tracking-tight font-dm-serif-display">
             The Adharv Times
           </span>
         </Link>
-        <span>Read &middot; Code &middot; Repeat</span>
+        <span className="hidden sm:inline">Read &middot; Code &middot; Repeat</span>
       </div>
     </header>
   );
